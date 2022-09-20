db95727dfcee554caeb7a6432106eb5b6a15777e
hasibmiraz <hasibmiraz09@gmail.com>
2022-09-20 22:37:52 +0600

    Create users successfully

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,6 +1,22 @@
 const User = require('../models/users.schema.js');
 
-exports.get = async (req, res) => {
+exports.getUsers = async (req, res) => {
   const users = await User.find();
-  res.status(201).json(users);
+  res.status(201).send({ success: true, users: users });
+};
+
+exports.createUser = async (req, res) => {
+  const { name, email, password } = req.body;
+  const oldUser = await User.findOne({ email });
+  if (oldUser) {
+    return res
+      .status(401)
+      .send({ success: false, message: 'This email is already in use' });
+  }
+
+  const newUser = new User({ name, email, password });
+  await newUser.save();
+  return res
+    .status(201)
+    .send({ success: true, message: 'User created successfully' });
 };
--- routes/user.route.js
+++ routes/user.route.js
@@ -2,6 +2,7 @@ const express = require('express');
 const userController = require('../controllers/user.controller.js');
 const router = express.Router();
 
-router.get('/users', userController.get);
+router.get('/users', userController.getUsers);
+router.post('/create', userController.createUser);
 
 module.exports = router;
